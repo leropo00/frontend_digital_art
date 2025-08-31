0e4f43e186edf4523c8a033fbcc0e929c4c79178
leropo00 <leropo00@gmail.com>
2025-08-31 20:14:55 +0200

    corrected home page to have consistent look with other pages

--- app/src/pages/HomeView.vue
+++ app/src/pages/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
   <main>
-     <div>
-        Entry point to application
+    <div class="shadow-lg rounded-lg overflow-hidden m-4 md:m-10 space-y-12">
+      <div class="border-b border-gray-900/10 pb-12">
+        <h2 class="text-lg font-bold text-gray-900 bg-gray-300 text-center py-1">Digital Art Assistant</h2>
+      </div>
     </div>
   </main>
 </template>
